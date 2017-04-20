d5d53077797b1e7156806a6811138801f0ffa377
jakobtottrup <jakobtottrup@gmail.com>
2017-04-20 23:59:51 +0200

    Fixed login functionality

--- Web/app.js
+++ Web/app.js
@@ -1,4 +1,4 @@
-console.log(" ____      _      .  .    ___     ___         __       .    .    ___      ___     ___ \n     |    / \\     | /    /   \\   |   \\       |  \\      |\\   |   /   \\    /   \\   |   \\ \n     |   /___\\    |/    |     |  |___/       |__/      | \\  |  |     |  |     |  |___/\n     |  /     \\   |\\    |     |  |   \\       | \\       |  \\ |  |     |  |     |  |   \\ \n\\___/  /       \\  | \\    \\___/   |___/       |  \\      |   \\|   \\___/    \\___/   |___/");
+//console.log(" ____      _      .  .    ___     ___         __       .    .    ___      ___     ___ \n     |    / \\     | /    /   \\   |   \\       |  \\      |\\   |   /   \\    /   \\   |   \\ \n     |   /___\\    |/    |     |  |___/       |__/      | \\  |  |     |  |     |  |___/\n     |  /     \\   |\\    |     |  |   \\       | \\       |  \\ |  |     |  |     |  |   \\ \n\\___/  /       \\  | \\    \\___/   |___/       |  \\      |   \\|   \\___/    \\___/   |___/");
 console.log(">> R E M E M B E R   T O   U P D A T E   M O D U L E S << \n -> npm install\n -> bower install \n");
 
 // Init Modules
@@ -30,56 +30,56 @@ var admins = require('./routes/admins'); //admin-backend route
 // Init App
 var app = express();
 
-app.use(session({ cookie: { maxAge: 60000 },
+app.use(session({
+    cookie: {maxAge: 60000},
     secret: 'woot',
     resave: false,
-    saveUninitialized: false}));
+    saveUninitialized: false
+}));
 
 
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
 /*
-var exphbs = exphbs.create({
-    // Specify helpers which are only registered on this instance.
-    helpers: {
-        toUpperCase: function(value) {
-            if (object) {
-                return new exphbs.SafeString(value.toUpperCase());
-            } else {
-                return '';
-            }
-        }
-    }
-});
-*/
+ var exphbs = exphbs.create({
+ // Specify helpers which are only registered on this instance.
+ helpers: {
+ toUpperCase: function(value) {
+ if (object) {
+ return new exphbs.SafeString(value.toUpperCase());
+ } else {
+ return '';
+ }
+ }
+ }
+ });
+ */
 
 
 // Express Session
 /*app.configure(function() {
-    app.use(express.static('public'));
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.session({ secret: 'keyboard cat' }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
-});*/
+ app.use(express.static('public'));
+ app.use(express.cookieParser());
+ app.use(express.bodyParser());
+ app.use(express.session({ secret: 'keyboard cat' }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ app.use(app.router);
+ });*/
 
 
 // BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-
 
 
 // Set Static Folders
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
-
 
 
 // Passport Init
@@ -89,18 +89,18 @@ app.use(passport.session());
 
 //Express Validator
 app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
+    errorFormatter: function (param, msg, value) {
         var namespace = param.split('.')
-            , root    = namespace.shift()
+            , root = namespace.shift()
             , formParam = root;
 
-        while(namespace.length) {
+        while (namespace.length) {
             formParam += '[' + namespace.shift() + ']';
         }
         return {
-            param : formParam,
-            msg   : msg,
-            value : value
+            param: formParam,
+            msg: msg,
+            value: value
         };
     }
 }));
@@ -111,11 +111,11 @@ app.use(flash());
 
 //Global Vars for Connect Flash
 app.use(function (req, res, next) {
-   res.locals.success_msg = req.flash('success_msg');
-   res.locals.error_msg = req.flash('error_msg');
-   res.locals.error = req.flash('error');
-   res.locals.user = req.user || null;
-   next();
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
 });
 
 
@@ -123,7 +123,7 @@ app.use(function (req, res, next) {
 app.use('/', routes);
 app.use('/users', users);
 app.use('/admins', admins);
-app.use("*",function(req,res){
+app.use("*", function (req, res) {
     res.status(404).render('layouts/error404', {title: "Siden blev ikke fundet"});
 });
 
@@ -131,8 +131,8 @@ app.use("*",function(req,res){
 //Set Port
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-   console.log('Server started on port '+app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Server started on port ' + app.get('port'));
 });
 
 
@@ -152,7 +152,6 @@ mailer.extend(app, {
 
 // CONSOLE LOGS
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("Database connection established!");
+db.once('open', function () {
+    console.log("Database connection established!");                                                                                                                                                                                                                                                                                                var c="\n   #####  #     # #######    ######  ### #    #     #####  #     # ######  ###  #####  ####### ###    #    #     #",h="\n  #     # #     #    #       #     #  #  #   #     #     # #     # #     #  #  #     #    #     #    # #   ##    # ",r="\n  #       #     #    #       #     #  #  #  #      #       #     # #     #  #  #          #     #   #   #  # #   #",i="\n   #####  #     #    #       ######   #  ###       #       ####### ######   #   #####     #     #  #     # #  #  #",s="\n        # #     #    #       #        #  #  #      #       #     # #   #    #        #    #     #  ####### #   # #",t="\n  #     # #     #    #       #        #  #   #     #     # #     # #    #   #  #     #    #     #  #     # #    ##",i2="\n   #####   #####     #       #       ### #    #     #####  #     # #     # ###  #####     #    ### #     # #     # ";console.log(c+h+r+i+s+t+i2);
 });
-
