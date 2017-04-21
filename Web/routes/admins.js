0af2c592d20b3555022a4dcfe810a8bbeeaee676
slimcdk <cs@slimc.dk>
2017-04-21 14:40:39 +0200

    Added some text

--- Web/routes/admins.js
+++ Web/routes/admins.js
@@ -95,6 +95,4 @@ router.get('/mails', function(req, res){
 });
 
 
-
-
 module.exports = router;
--- Web/routes/frontend.js
+++ Web/routes/frontend.js
@@ -150,6 +150,7 @@ passport.use(new LocalStrategy(
         })
     }
 ));
+
 passport.serializeUser(function(user, done) {
     done(null, user.id);
     //console.log("USER: "+user.username+" | ID: "+user.id);
