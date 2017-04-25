f844480868f41f4a29cc86b16000ae7f27319295
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-04-25 13:04:23 +0200

    countdown moved to javascript

--- Web/public/js/seatmap_admin.js
+++ Web/public/js/seatmap_admin.js
@@ -2,3 +2,18 @@
  * Created by ste on 24-04-2017.
  */
 document.write("seatmap admin script initialized");
+
+function getSeatData(){
+    return $.ajax({
+        type: 'GET',
+        url: "/api/seats",
+        dataType: "json"
+    }).done(function(data){
+        groupData = data;
+    });
+}
+
+$.when(getSeatData()).done(function(){
+    console.log(groupData);
+});
+
--- Web/routes/api.js
+++ Web/routes/api.js
@@ -111,4 +111,7 @@ router.get('/users/admin', /*ensureAdminAuthenticated,*/ function(req, res) {
 });
 
 
+
+
+
 module.exports = router;
