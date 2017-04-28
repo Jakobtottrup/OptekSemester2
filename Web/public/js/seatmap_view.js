d646f58fe1d38a4adc95fc6618cb167ad8219de6
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-04-28 22:55:22 +0200

    Seatmap reads username

--- Web/public/js/seatmap_view.js
+++ Web/public/js/seatmap_view.js
@@ -13,6 +13,7 @@ var ctx = canvas.getContext('2d');
 /*** ****** ***/
 
 var temp;
+m_index = -1;
 
 mycanvas = {width: 640, height: 320};
 
@@ -112,32 +113,6 @@ function setVariables() {
 /** set functions **/
 /*** ********** ****/
 
-function getSeatColor(seat) {
-    //void
-    if (seat.type == 0) {
-        return "gray";
-    }
-    //wall
-    if (seat.type == 1) {
-        return "black";
-    }
-    //seat
-    if (seat.type == 2) {
-        return "green";
-    }
-    //admin area
-    if (seat.type == 3) {
-        return "darkred";
-    }
-    //kiosk
-    if (seat.type == 4) {
-        return "blue";
-    }
-    //error
-    console.log(seat);
-    return "purple";
-}
-
 function updateInfoBox(index) {
     var seat = seatmap.seats[index];
     var infoText1 = "";
@@ -151,6 +126,7 @@ function updateInfoBox(index) {
         } else {
             var userinfo = infoFromID(seat.userid);
             infoText2 = "Navn: " + userinfo.username;
+            //tjek pladsgruppe-array her for hver brugers gruppe
         }
     } else if (seat.type == 3) {
         infoText1 = "Administration";
@@ -167,6 +143,95 @@ function infoFromID(id) {
     return (userIDs.filter(function(a){ return a._id == id })[0]);
 }
 
+/*** ******** ***/
+/** draw object */
+/*** ******** ***/
+
+function drawPixel(x, y, seat, hover) {
+    if (seat.type == 1) {
+        drawWall(x, y);
+    } else if (seat.type == 2) {
+        drawSeat(x, y, seat.userid, hover);
+    } else if (seat.type == 3) {
+        drawAdmin(x, y);
+    } else if (seat.type == 4) {
+        drawShop(x, y);
+    }
+}
+
+function drawSeat(x, y, id, hover) {
+    var offset = 6; //%
+    offset = (seat_size / 100) * offset;
+    var newsize = seat_size - (2 * offset);
+    var halfsize = newsize / 2;
+
+    var _x = x * seat_size + offset; //x to left
+    var _y = y * seat_size + offset; //y to top
+    var x_ = (x+1) * seat_size - offset; //x to right
+    var y_ = (y+1) * seat_size - offset; //y to bottom
+    var rad = 7; //cornor radius
+
+    /*
+    1 2 3
+    8   4
+    7 6 5
+     */
+
+    var cor1 = {x: _x, y: _y};
+    var cor2 = {x: _x + halfsize, y: _y};
+    var cor3 = {x: x_, y: _y};
+    var cor4 = {x: x_, y: _y + halfsize};
+    var cor5 = {x: x_, y: y_};
+    var cor6 = {x: _x + halfsize, y: y_};
+    var cor7 = {x: _x, y: y_};
+    var cor8 = {x: _x, y: _y + halfsize};
+
+    ctx.beginPath();
+    ctx.moveTo(cor2.x, cor2.y);
+    ctx.arcTo(cor3.x, cor3.y, cor4.x, cor4.y, rad);
+    ctx.arcTo(cor5.x, cor5.y, cor6.x, cor6.y, rad);
+    ctx.arcTo(cor7.x, cor7.y, cor8.x, cor8.y, rad);
+    ctx.arcTo(cor1.x, cor1.y, cor2.x, cor2.y, rad);
+    ctx.lineTo(cor2.x, cor2.y);
+    ctx.closePath();
+
+    if (id == 0) {
+        //if seat is free
+        if (hover) {
+            ctx.fillStyle = "green";
+        } else {
+            ctx.fillStyle = "darkgreen";
+        }
+    } else {
+        //if seat is taken
+        if (hover) {
+            ctx.fillStyle = "red";
+        } else {
+            ctx.fillStyle = "darkred";
+        }
+    }
+
+    ctx.strokeStyle = "white";
+
+    ctx.fill();
+    ctx.stroke();
+}
+
+function drawWall(x, y) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(x * seat_size, y * seat_size, seat_size, seat_size);
+}
+
+function drawAdmin(x, y) {
+    ctx.fillStyle = "darkred";
+    ctx.fillRect(x * seat_size, y * seat_size, seat_size, seat_size);
+}
+
+function drawShop(x, y) {
+    ctx.fillStyle = "darkblue";
+    ctx.fillRect(x * seat_size, y * seat_size, seat_size, seat_size);
+}
+
 /*** *********** ***/
 /** draw on screen */
 /*** *********** ***/
@@ -179,10 +244,9 @@ function drawScreen() {
 
     for (var i = 0; i < seatmap.seats.length; i++) {
         if (seatmap.seats[i].type != 0) {
-            ctx.fillStyle = getSeatColor(seatmap.seats[i]);
             var _x = i % seatmap.room_width;
             var _y = (i - _x) / seatmap.room_width;
-            ctx.fillRect(_x * seat_size, _y * seat_size, seat_size, seat_size);
+            drawPixel(_x, _y, seatmap.seats[i], (m_index == i));
         }
     }
 }
@@ -217,7 +281,7 @@ canvas.addEventListener("mousemove", function(e){
         my = 0;
     }
 
-    var m_index = mx + (my * seatmap.room_width);
+    m_index = mx + (my * seatmap.room_width);
 
     //console.log(m_index);
 
