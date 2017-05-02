d4157af89b83f6237dffc2178d3ea1982e8a5c6b
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-05-02 23:21:29 +0200

    Tournament-view not finished, but a lot of work done in order to make it happen

--- Web/public/js/tournament_view.js
+++ Web/public/js/tournament_view.js
@@ -15,5 +15,27 @@ function getTourData(){
 }
 
 $.when(getTourData()).done(function() {
-    console.log(tour);
+    createTours();
 });
+
+function createTours() {
+    for (i = 0; i < tour.length; i++) {
+        var output = "";
+        output += "<div class='tour-div'>";
+        output += "<div class='tour-title'><h2>" + tour[i].name + "</h2></div>";
+        output += "<div class='tour-cover'><img src='" + tour[i].coverImage + "'></div>";
+        output += "<div class='tour-countdown'><p>Turneringen begynder " + tour[i].openingDate + "</p></div>";
+        output += "<div class='tour-price-list'>";
+        for (var j = 0; j < tour[i].prizes.length; j++) {
+            output += "<div class='tour-price'>";
+            output += "<p>" + (j+1) + ". Plads</p>";
+            output += "<img src='" + tour[i].prizes[j].p_image + "'>";
+            output += "<p>" + tour[i].prizes[j].p_name + "</p>";
+            output += "</div>";
+        }
+        output += "</div>";
+        output += "</div>";
+
+        $('#all_tours').append(output);
+    }
+}
