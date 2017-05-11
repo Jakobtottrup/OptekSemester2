420a4590eddb4936a6571006b4fa0b625eb65fa2
slimcdk <cs@slimc.dk>
2017-05-11 16:36:16 +0200

    Update to user search option

--- Web/public/js/userinfo/user-userview.js
+++ Web/public/js/userinfo/user-userview.js
@@ -3,6 +3,7 @@
  */
 
 $.when(getGroupData(), getUsersData()).done(function() {
+    console.log(groupData);
     let output = "";
     for (let i = 0; i < usersData.length; i++){
         output += "<tr>";
@@ -23,9 +24,9 @@ $.when(getGroupData(), getUsersData()).done(function() {
 
 // check if user is in group already - returns boolean
 function seatGroup(id) {
+    console.log(id);
     for (let i = 0; i < groupData.length; i++) {
-        console.log(groupData[i]);
-        for (let j = 0; j < groupData[i].members.length; j++) {
+        for (let j = 0; j < groupData[i].members.length; i++){
             if (groupData[i].leaderID === id || groupData[i].members[j] === id) {
                 console.log(groupData[i]);
                 return groupData[i].groupName;
@@ -35,22 +36,29 @@ function seatGroup(id) {
         }
     }
 }
-/*
-function seatGroup(id) {
-    console.log(id);
-    for(i=0; i < groupData.length; i++){
-        for(j=0; j < groupData[i].members.length; j++) {
-            if (groupData[i].isLeader === id || groupData[i].members[j] === id) {
-                return groupData[i].groupName;
-            } else {
-                return "Bruger er ikke i gruppe"
-            }
-        }
-    }
-}
-*/
 
 
 function userSeat(id) {
 
+}
+
+function searchTable() {
+    // Declare variables
+    let input, filter, table, tr, td, i;
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("user_table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
