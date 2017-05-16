6d04b5990b02bff54749ce524dfec0dfe382d883
Sebastian Andresen <sebastian.sommersted@live.dk>
2017-05-16 14:16:31 +0200

    Countdown counts to 00:00:00:00 and nothing more

--- Web/public/js/index_countdown.js
+++ Web/public/js/index_countdown.js
@@ -19,72 +19,30 @@ $.when(getEventData()).done(function() {
 
     getCountdown();
 
-    //setInterval(function () { getCountdown(); }, 1000);
-
-
-
-
-
-    /* this code is instead of setInterval */
-
-    function interval(duration, fn){
-        this.baseline = undefined
-
-        this.run = function(){
-            if(this.baseline === undefined){
-                this.baseline = new Date().getTime();
-            }
-            fn()
-            var end = new Date().getTime();
-            this.baseline += duration;
-
-            var nextTick = duration - (end - this.baseline);
-            if(nextTick<0){
-                nextTick = 0;
-            }
-            (function(i){
-                i.timer = setTimeout(function(){
-                    i.run(end);
-                }, nextTick)
-            }(this))
-        }
-
-        this.stop = function(){
-            clearTimeout(this.timer);
-        }
-    }
-
-    var timer = new interval(1000, function(){
-        getCountdown();
-        //console.log(new Date().getTime())
-    });
-
-    timer.run();
-
-    /* this code is instead of setInterval */
-
-
-
-
-
-
-
-
+    var timer = setInterval(function () { getCountdown(); }, 1000);
 
     function getCountdown(){
 
         // find the amount of "seconds" between now and target
         let current_date = new Date().getTime();
-        let seconds_left = (target_date - current_date) / 1000;
+        let seconds_left = Math.floor((target_date - current_date) / 1000 - 7200); //timezone manually withdrawed
 
-        days = pad( parseInt(seconds_left / 86400) );
-        seconds_left = seconds_left % 86400;
+        if (seconds_left <= 0) {
+            days = "00";
+            hours = "00";
+            minutes = "00";
+            seconds = "00";
+            clearInterval(timer);
+        } else {
+            days = pad( parseInt(seconds_left / 86400) );
+            seconds_left = seconds_left % 86400;
 
-        hours = pad( parseInt(seconds_left / (3600 + 7200) ) );
-        seconds_left = seconds_left % 3600;
+            hours = pad( parseInt(seconds_left / (3600 + 7200) ) );
+            seconds_left = seconds_left % 3600;
 
-        minutes = pad( parseInt(seconds_left / 60) );
-        seconds = pad( parseInt( seconds_left % 60 ) );
+            minutes = pad( parseInt(seconds_left / 60) );
+            seconds = pad( parseInt( seconds_left % 60 ) );
+        }
 
         // format countdown string + set tag value
         countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
