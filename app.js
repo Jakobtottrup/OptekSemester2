37448831b2e6eb2f835843cfb309d9497a5f1b57
Christian <cs@slimc.dk>
2017-08-30 13:56:45 +0200

    Changed port

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.use("*", function (req, res) {
 
 
 //Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 2000));
 
 app.listen(app.get('port'), "0.0.0.0", function () {
     console.log('Server started on port ' + app.get('port'));
